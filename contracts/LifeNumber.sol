6d84de6b835ec723ab2648f600f43d0d0592c80e
"uphoon" <"uphoon@gmail.com">
2022-04-13 17:01:20 +0900

    feature rollball

--- contracts/LifeNumber.sol
+++ contracts/LifeNumber.sol
@@ -1,43 +1,60 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.7.0 <0.9.0;
 
 /** 
- * @title Ballot
- * @dev Implements voting process along with vote delegation
+ * @title Life Number
+ * @dev 
  */
 contract LifeNumber {
 
-   uint[] numbers ;
-
    address owner;
 
+   uint fee = 1 ether;
+   //uint randNonce = 0;
+   //uint[] numbers ;
+   //mapping(address => uint8[]) numbers;
+   
+
    constructor() {
-      owner = msg.sender;
+      owner = msg.sender;  // Deployer
    }
 
-   function getNumbers() public view returns(uint[] memory) {
 
-      return numbers;
+
+   function getNumbers() external payable {
+
+      //TODO : payable 
+      require(msg.value == fee);
    } 
 
-   function rollball() public  {
-
-      require( numbers.length < 6 );
-
-      uint n = _randMod();
-
-      if(_isPutNumber(n)){
-         rollball();
-      } else {
-         numbers.push(n);
-      }
+   function fallback() public returns(uint[] memory) {   // receive
+      return _rollball();
    }
 
-   function _isPutNumber(uint _n) private view returns (bool) {
+
+   // Run numbers
+   function _rollball() private view returns (uint[] memory) {
+
+      uint[] memory nums = new uint[](6);
+      uint nonce = 0;
+      uint n ;
+
+      for(uint8 i=0; i < 6 ; i++) {
+         do {
+            n = _randMod(nonce);
+            nonce++;
+         } while(_isPutNumber(nums, n));
+         nums[i] = n;
+      }
+      return nums;
+   }
+
+
+   // Check the same number in numbers[]
+   function _isPutNumber(uint[] memory _nums, uint _n) private pure returns (bool) {
       bool r = false;
-      for(uint i = 0; i < numbers.length; i++){
-         if(_n == numbers[i] ){
+      for(uint i = 0; i < _nums.length; i++){
+         if(_n == _nums[i] ){
             r = true;
             break;
          }
@@ -46,9 +63,30 @@ contract LifeNumber {
    }
 
 
-   function _randMod() private view returns(uint) {
-      uint randNonce = 0;
-      return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % 46;
+   // make Random Number
+   function _randMod(uint _nonce) private view returns(uint) {
+      return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, _nonce))) % 46;
    }
+
+
+   // Run numbers
+   // function rollball() public  {
+
+   //    require( numbers.length < 6 );
+
+   //    uint n = _randMod(randNonce);
+   //    randNonce++;
+
+   //    if(_isPutNumber(n)){
+   //       rollball();
+   //    } else {
+   //       numbers.push(n);
+   //       if(numbers.length < 6){
+   //          rollball();
+   //       }
+   //    }
+   // }
+
+
 
 }
